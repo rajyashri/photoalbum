90855f75232631d0074fa9b15361e274da0cbe61
v kaiser pendergrast <v.kaiser.pendergrast@gmail.com>
2015-03-03 17:33:12 -0500

    Implemented all methods in CmdView and UserController, with the exception of login.

--- src/cs213/photoAlbum/control/UserController.java
+++ src/cs213/photoAlbum/control/UserController.java
@@ -5,9 +5,16 @@
 
 package cs213.photoAlbum.control;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 import cs213.photoAlbum.model.IUser;
 import cs213.photoAlbum.model.User;
-import cs213.photoAlbum.simpleview.InteractiveView;
 
 public class UserController {
 
@@ -16,31 +23,147 @@ public class UserController {
 	}
 	
 	/**
-	 * 
-	 * @param userName
-	 * @param passWord
+	 * @param username
 	 * @return true if successfully created a new user
 	 */
-	public boolean createUser(String userName,String passWord){
-		return false;
+	public boolean addUser(String userid, String name){
+		if(userExists(userid)) {
+			return false;	
+		}
+
+		User user = new User(userid, name);
+		try {
+			FileOutputStream fileOut = new FileOutputStream(getDataDirectory() + "/" + userid);
+			ObjectOutputStream objOut = new ObjectOutputStream(fileOut);
+
+			objOut.writeObject(user);
+
+			objOut.close();
+			fileOut.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Deletes a user
+	 * @return true if user existed and was deleted
+	 */
+	public boolean deleteUser(String userid) {
+		if(!userExists(userid)) {
+			return false;
+		}
+
+		try {
+			File f = new File(getDataDirectory() + "/" + userid);
+			f.delete();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+
+		return true;
 	}
 	
 	/**
-	 * checks if user exist and returns true 
+	 * Checks if user exists
+	 * @return true if user file exists in data/
 	 */
 	public boolean userExists(String userid) {
-		// TODO Auto-generated method stub
-		return false;
+		File f = new File(getDataDirectory() + "/" + userid);
+		return f.exists();
 	}
-	
-	/**
-	 * 
-	 * @param userName
-	 * @returns the list of users on record
-	 */
-	public IUser listUsers() {
+
+	public IUser getUser(String userid) {
+		String dataDir = getDataDirectory();
+
+		try {
+			File dir = new File(dataDir);		
+			FileInputStream fileIn = new FileInputStream(dir + "/" + userid);
+			ObjectInputStream objIn = new ObjectInputStream(fileIn);
+
+			Object obj = objIn.readObject();
+			objIn.close();
+			if(obj instanceof IUser) {
+				return (IUser) obj;
+			} else {
+				System.out.println("This is not an IUser object!");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		return null;
 	}
 	
+	/**
+	 * Get a list of users
+	 * @returns the list of users in data/
+	 */
+	public List<IUser> listUsers() {
+		List<IUser> users = new ArrayList<IUser>();
+
+		String dataDir = getDataDirectory();
+
+		try {
+			File dir = new File(dataDir);		
+			String[] files = dir.list();
+			
+			for(String file : files) {
+				// Ignore hidden files, current directory, and parent directory
+				if(!file.startsWith(".")) {
+					FileInputStream fileIn = new FileInputStream(dir + "/" + file);
+					ObjectInputStream objIn = new ObjectInputStream(fileIn);
+
+					Object obj = objIn.readObject();
+					if(obj instanceof IUser) {
+						users.add((IUser) obj);
+					} else {
+						System.out.println("This is not an IUser object!");
+					}
+
+					objIn.close();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+
+		return users;
+	}
+	
+
+	/**
+	 * Get the directory data/ directory path
+	 */
+	private String getDataDirectory() {
+		try {
+			File f = new File(".");
+			String path = f.getAbsolutePath().toString();
+			if(path.endsWith("/.")) {
+				path = path.substring(0, path.length() - 2);
+			}
+			if(path.endsWith("/bin")) {
+				path = path.substring(0, path.length() - 4);
+			}
+
+			path = path + "/data";
+
+			f = new File(path);
+			if(!f.exists()) {
+				f.mkdir();
+			}
+
+			return path;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return "../data";
+	}
 	
 }
