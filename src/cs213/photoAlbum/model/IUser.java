a648e4bcc48ef750474bbff129cafe78b232e0d6
Rajyashri Vasudevamoorthy <rajyashriv@gmail.com>
2015-04-13 12:14:15 -0400

    Updated AdminView

--- src/cs213/photoAlbum/model/IUser.java
+++ src/cs213/photoAlbum/model/IUser.java
@@ -1,4 +1,4 @@
-<<<<<<< local
+
 /**
  * @author Victor Kaiser-Pendergrast
  * @author Rajyashri Vasudevamoorthy
@@ -76,58 +76,3 @@ public interface IUser extends Comparable<IUser> {
 	public IAlbum getAlbum(String name);
 
 }
-=======
-/**
- * @author Victor Kaiser-Pendergrast
- */
-
-package cs213.photoAlbum.model;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public interface IUser extends Comparable<IUser> {
-
-	/**
-	 * @return the userId
-	 */
-	public String getUserId();
-
-	/**
-	 * @param userId the userId to set
-	 */
-	public void setUserId(String userId);
-
-	/**
-	 * @return the fullName
-	 */
-	public String getFullName();
-
-	/**
-	 * @param fullName the fullName to set
-	 */
-	public void setFullName(String fullName);
-
-	/**
-	 * Add an album to this user
-	 * @return true if the album was added, false if a duplicate album exists
-	 */
-	public boolean addAlbum(IAlbum a);
-
-	/**
-	 * @return true if this user has this album
-	 */
-	public boolean containsAlbum(IAlbum a);
-
-	public boolean containsAlbum(String albumName);
-
-	/**
-	 * Remove an album and its photos
-	 * @return true if the album was removed, false if the album did not exist
-	 */
-	public boolean removeAlbum(IAlbum a);
-	
-	public List<IAlbum> getAlbums();
-
-}
->>>>>>> other
--- src/cs213/photoAlbum/simpleview/CmdView.java
+++ src/cs213/photoAlbum/simpleview/CmdView.java
@@ -1,4 +1,4 @@
-<<<<<<< local
+
 /**
  * @author Victor Kaiser-Pendergrast
  * @author Rajyashri Vasudevamoorthy
@@ -86,81 +86,3 @@ public class CmdView {
 	}
 
 }
-=======
-package cs213.photoAlbum.simpleview;
-
-import java.util.List;
-
-import cs213.photoAlbum.control.UserController;
-import cs213.photoAlbum.model.IUser;
-
-public class CmdView {
-
-	public static void main(String[] argv) {
-		UserController controller = new UserController();
-
-		// Determine possible commands by arguments
-		switch(argv.length) {
-			case 0:
-				printUsage();
-				break;
-			case 1:
-				if("listusers".equals(argv[0])) {
-					List<IUser> users = controller.listUsers();
-
-					if(users.isEmpty()) {
-						System.out.println("no users exist");
-					}
-
-					for(IUser user : users) {
-						System.out.println(user.getUserId());
-					}
-				} else {
-					printUsage();
-				}
-				break;
-			case 2:
-				if("deleteuser".equals(argv[0])) {
-					if(controller.deleteUser(argv[1])) {
-						System.out.println("deleted user " + argv[1]);
-					} else {
-						System.out.println("user " + argv[1] + " does not exist");
-					}
-				}  else if ("login".equals(argv[0])) {
-					// TODO launch interactive mode
-					 String loginId = argv[1];
-					 if(controller.userExists(loginId)){
-						 InteractiveView userMode = new InteractiveView(controller.getUser(loginId));
-						    userMode.userEdit();
-					 }
-					  
-				}
-				break;
-			case 3:
-				if("adduser".equals(argv[0])) {
-					if(controller.addUser(argv[1], argv[2])) {
-						System.out.println("created user " + argv[1] + " with name " + argv[2]);
-					} else {
-						System.out.println("user " + argv[1] + " already exists with name " + controller.getUser(argv[1]).getFullName());
-					}
-				} else {
-					printUsage();
-				}
-				break;
-			default:
-				printUsage();
-				break;
-		}	
-	}
-
-
-	private static void printUsage() {
-		System.out.println("Usage:");
-		System.out.println("java cs213.photAlbum.simpleview.CmdView listusers");
-		System.out.println("java cs213.photAlbum.simpleview.CmdView adduser <user id> <user's name>");
-		System.out.println("java cs213.photAlbum.simpleview.CmdView deleteuser <user id>");
-		System.out.println("java cs213.photAlbum.simpleview.CmdView login <user id>");
-	}
-
-}
->>>>>>> other
